5ee6162a5eb3f5e959a1cf0a2b1d473020261aa8
Adam Ard <adam.ard@vivintsolar.com>
2020-06-04 09:35:13 -0600

    some cleanup and notes

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import sys
 
-# NOTE: call stack for long strings might be a problem
-#   but the limit is 100 in spec, so we are ok
-def n_flips(s):
+# NOTE: this can be done recursively but the
+#   call stack for long strings might overflow
+#   the spec says the  limit is 100, so it should be ok
+#       The iterative approace is also faster so I am
+#       using that one. I am leaving this here for refrence
+#       because it very clear
+def n_flips_recurse(s):
     if len(s) == 1:
         return 1 if s[0] == '-' else 0
 
     while(True):
         if s[0] != s[1]:
-            return n_flips(s[1:]) + 1
-        return n_flips(s[1:])
+            return n_flips_recurse(s[1:]) + 1
+        return n_flips_recurse(s[1:])
 
 
-def get_num_flips(s):
+def n_flips(s):
     if len(s) == 1:
         return 1 if s[0] == '-' else 0
 
@@ -30,16 +34,11 @@ def get_num_flips(s):
 def main():
     with open(sys.argv[1]) as f:
         num_tests = f.readline()
-
-        # NOTE: if the data set if large, this could overflow
-        # maybe stream, and reuse the same string, pre allocated to make of 100
-        #  while using less memory, this will be slower
         tests = f.read()
 
     for i, test in enumerate(tests.split()):
-        print('Case #%s: %s %s' % (i+1, get_num_flips(test), n_flips(test)))
+        print('Case #%s: %s %s' % (i+1, n_flips(test)))
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
