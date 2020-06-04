84f959dc9a8f8fc845827d92187af9d2708e3059
Adam Ard <adam.ard@vivintsolar.com>
2020-06-03 22:15:04 -0600

    add a recursive version

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import sys
 
+# TODO: call stack for long strings might be a problem
+#   but the limit is 100
+def n_flips(s):
+    if len(s) == 1:
+        return 1 if s[0] == '-' else 0
+
+    while(True):
+        if s[0] != s[1]:
+            return n_flips(s[1:]) + 1
+        return n_flips(s[1:])
+
 def get_num_flips(s):
     if len(s) == 1:
         if s[0] == '-':
@@ -29,10 +40,12 @@ def get_num_flips(s):
 def main():
     with open(sys.argv[1]) as f:
         num_tests = f.readline()
+
+        # NOTE: if the data set if large, this could overflow
         tests = f.read()
 
     for i, test in enumerate(tests.split()):
-        print('Case #%s: %s' % (i+1, get_num_flips(test)))
+        print('Case #%s: %s %s' % (i+1, get_num_flips(test), n_flips(test)))
 
 if __name__ == "__main__":
     # execute only if run as a script
